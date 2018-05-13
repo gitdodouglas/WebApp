e97cf5557a7ae70a75e6257b2aa7a2fc6496c7c6
Douglas Inácio <douglasinacio6@gmail.com>
2018-05-13 19:39:18 -0300

    Criação do projeto e conexão com Heroku DB

---
+++ README.md
@@ -0,0 +1,2 @@
+# WebApp
+Projeto desenvolvido para a disciplina de Tópicos Especiais em Informática (Programação Funcional em Haskell) na Faculdade de Tecnologia da Baixada Santista.
---
+++ src/Handler/Home.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+module Handler.Home where
+
+import Import
+import Network.HTTP.Types.Status
+import Database.Persist.Postgresql
+
+getHomeR :: Handler Html
+getHomeR = undefined

6672c3879eaf18a0224c58a32295d431d7ea92d8
Romulo Garofalo <romulogarofalo@gmail.com>
2018-05-22 17:50:59 -0300

    idk

--- front-end/src/app/login.service.ts
+++ front-end/src/app/login.service.ts
@@ -9,6 +9,8 @@ export class LoginService {
   constructor(private http: HttpClient) { }
 
   async getLogin(login: string, senha: string) {
+    // console.log('https://reqres.in/api/login');
+    // return await this.http.post('https://reqres.in/api/login', { 'email': login, 'password': senha }).toPromise();
     return await this.http.post('http://localhost:8080/login', [ login, senha ]).toPromise();
   }
 }

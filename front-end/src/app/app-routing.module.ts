79f06211fedbe78fdb4fa2475d52ef40151f3000
Romulo Garofalo <romulogarofalo@gmail.com>
2018-05-24 16:45:52 -0300

    routes of the components and add angular-webstorage-service

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes, RouterState } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { MenuComponent } from './menu/menu.component';
+import { CompartilhadasComponent } from './compartilhadas/compartilhadas.component';
+import { ListasComponent } from './listas/listas.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
-
-
+  { path: 'register', component: RegisterComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'compartilhadas', component: CompartilhadasComponent },
+  { path: 'listas', component: ListasComponent }
 ];
 
 
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -6,17 +6,25 @@ import { AppRoutingModule } from './/app-routing.module';
 import { LoginComponent } from './login/login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RegisterComponent } from './register/register.component';
+import { MenuComponent } from './menu/menu.component';
+import { ListasComponent } from './listas/listas.component';
+import { CompartilhadasComponent } from './compartilhadas/compartilhadas.component';
+import { StorageServiceModule } from 'angular-webstorage-service';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    MenuComponent,
+    ListasComponent,
+    CompartilhadasComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StorageServiceModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

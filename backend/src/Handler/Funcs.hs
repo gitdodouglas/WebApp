76f828065636ee131e340a15eae646acec992934
Romulo Garofalo <romulogarofalo@gmail.com>
2018-06-03 04:58:24 -0300

    done: function to get the token from the header

--- backend/src/Handler/Funcs.hs
+++ backend/src/Handler/Funcs.hs
@@ -1,14 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Handler.Funcs where
 
-import Yesod
-import Foundation
-import Data.Text as T
-import Text.Show as S
+import Import
+import qualified Data.Text as T
+import Network.Wai as NW
+import qualified Data.Maybe as M
+import qualified Data.ByteString.Char8 as BS
 
 data HttpMethod = OPTIONS | GET | POST | PUT | PATCH | DELETE deriving Show
 
 anyOriginIn :: [HttpMethod] -> Handler ()
 anyOriginIn methods = do
     addHeader (T.pack "Access-Control-Allow-Origin") (T.pack "*")
-    addHeader (T.pack "Access-Control-Allow-Methods") $ T.intercalate (T.pack ", ") Prelude.$ Prelude.map T.pack Prelude.$ Prelude.map Prelude.show methods
-    addHeader (T.pack "Access-Control-Allow-Headers") (T.pack "Content-Type")
+    addHeader (T.pack "Access-Control-Allow-Methods") $ T.intercalate (T.pack ", ") $ Prelude.map T.pack $ Prelude.map Prelude.show methods
+    addHeader (T.pack "Access-Control-Allow-Headers") (T.pack "*")
+
+-- getTokenHeader :: UsuarioId
+getTokenHeader :: Handler Text
+getTokenHeader = do
+    a <- waiRequest
+    listaHeader <- return $ NW.requestHeaders a
+    return $ T.pack $ BS.unpack $ M.fromJust $ Prelude.lookup "key" listaHeader

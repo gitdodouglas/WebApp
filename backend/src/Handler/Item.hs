91918b775894233b3cb22044ae924c61e4313a3e
Romulo Garofalo <romulogarofalo@gmail.com>
2018-06-05 14:29:33 -0300

    changed in the way json returned for the frontend :octocat:

--- backend/src/Handler/Item.hs
+++ backend/src/Handler/Item.hs
@@ -3,20 +3,33 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DeriveAnyClass #-}
 module Handler.Item where
 
 import Import
+import Handler.Funcs as F
 import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
+import qualified Data.Text as T
 
+-----------------------------------------------
+optionsItensListaR :: ListaId -> Handler ()
+optionsItensListaR _ = anyOriginIn [ F.OPTIONS, F.GET ]
+-----------------------------------------------
 -- busca todos os itens de uma lista
 getItensListaR :: ListaId -> Handler Value
 getItensListaR lid = do
-    _ <- runDB $ get404 lid
-    itemP <- runDB $ selectList [ItemProdutoListaid ==. lid] []
-    prodid <- return $ fmap(\ls -> itemProdutoProdutoid $ entityVal ls) itemP
-    produto <- runDB $ selectList [ProdutoId <-. prodid] []
-    sendStatusJSON ok200 (object ["resp" .= object(["produto" .= produto, "item" .= itemP]) ])
+    anyOriginIn [ F.OPTIONS, F.GET ]
+    token <- getTokenHeader
+    maybeUser <- runDB $ selectFirst [UsuarioToken ==. token] []
+    case maybeUser of
+        Just (Entity uid usuario) -> do
+            _ <- runDB $ get404 lid
+            itemP <- runDB $ selectList [ItemProdutoListaid ==. lid] []
+            prodid <- return $ fmap(\ls -> itemProdutoProdutoid $ entityVal ls) itemP
+            produto <- runDB $ selectList [ProdutoId <-. prodid] []
+            sendStatusJSON ok200 (object ["resp" .= object(["produtoLista" .= (zipWith unificaItemProduto (map entityVal itemP) (map entityVal produto))])])
+        _ -> sendStatusJSON forbidden403 (object [ "resp" .= ("acao proibida"::Text)])
 
 -- atualiza todos os campos de um item de produto pelo id
 putOpItemR :: ItemProdutoId -> Handler Value
@@ -51,3 +64,18 @@ postCadItemR = do
     item <- requireJsonBody :: Handler ItemProduto
     ipid <- runDB $ insert item
     sendStatusJSON created201 (object ["resp" .= fromSqlKey ipid])
+
+data ItemProd = ItemProd {
+    nome::Text,
+    valor::Double,
+    qtItem::Int,
+    listaid::ListaId,
+    produtoid::ProdutoId
+    } deriving Show
+
+unificaItemProduto :: ItemProduto -> Produto -> ItemProd
+unificaItemProduto (ItemProduto vluni qti _ lid pid) (Produto nome) = (ItemProd nome vluni qti lid pid)
+
+instance ToJSON ItemProd where
+    toJSON (ItemProd nome vluni qti lid pid) =
+        object ["nome" .= nome, "vlunitario" .= vluni, "qtditem" .= qti, "listaId" .= lid, "produtoId" .= pid]

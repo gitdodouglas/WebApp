77c98522f7e1df01cf59e8cf3c8a721b099a7b7e
Romulo Garofalo <romulogarofalo@gmail.com>
2018-06-08 19:15:47 -0300

    delete de produto de uma lista especifica :blue_book:

--- backend/src/Handler/Item.hs
+++ backend/src/Handler/Item.hs
@@ -11,10 +11,14 @@ import Handler.Funcs as F
 import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
 import qualified Data.Text as T
+import qualified Data.Maybe as M
 
 -----------------------------------------------
 optionsItensListaR :: ListaId -> Handler ()
 optionsItensListaR _ = anyOriginIn [ F.OPTIONS, F.GET ]
+
+optionsOpItemR :: ListaId -> ProdutoId -> Handler () 
+optionsOpItemR _ _ = anyOriginIn [ F.OPTIONS, F.DELETE ]
 -----------------------------------------------
 -- busca todos os itens de uma lista
 getItensListaR :: ListaId -> Handler Value
@@ -26,32 +30,35 @@ getItensListaR lid = do
         Just (Entity uid usuario) -> do
             _ <- runDB $ get404 lid
             itemP <- runDB $ selectList [ItemProdutoListaid ==. lid] []
-            prodid <- return $ fmap(\ls -> itemProdutoProdutoid $ entityVal ls) itemP
+            prodid <- return $ map (\ls -> itemProdutoProdutoid $ entityVal ls) itemP
             produto <- runDB $ selectList [ProdutoId <-. prodid] []
             sendStatusJSON ok200 (object ["resp" .= object(["produtoLista" .= (zipWith unificaItemProduto (map entityVal itemP) (map entityVal produto))])])
         _ -> sendStatusJSON forbidden403 (object [ "resp" .= ("acao proibida"::Text)])
 
 -- atualiza todos os campos de um item de produto pelo id
+{-
 putOpItemR :: ItemProdutoId -> Handler Value
 putOpItemR ipid = do
     _ <- runDB $ get404 ipid
     itemNovo <- requireJsonBody :: Handler ItemProduto
     runDB $ replace ipid itemNovo
     sendStatusJSON noContent204 (object [])
-
+-}
 -- deleta um item de produto pelo id
-deleteOpItemR :: ItemProdutoId -> Handler Value
-deleteOpItemR ipid = do
-    _ <- runDB $ get404 ipid
-    runDB $ delete ipid
-    sendStatusJSON noContent204 (object [])
+deleteOpItemR :: ListaId -> ProdutoId -> Handler Value
+deleteOpItemR lid pid = do
+    anyOriginIn [ F.OPTIONS, F.DELETE ]
+    listaProd <- runDB $ selectFirst [ItemProdutoListaid ==.lid, ItemProdutoProdutoid ==. pid] []
+    runDB $ delete $ entityKey $ M.fromJust listaProd
+    sendStatusJSON noContent204 ()
 
 -- recupera um item de produto pelo id
+{-
 getOpItemR :: ItemProdutoId -> Handler Value
 getOpItemR ipid = do
     item <- runDB $ get404 ipid
     sendStatusJSON ok200 (object ["resp" .= item])
-
+-}
 -- recupera todos os itens de produto
 getRecItemR :: Handler Value
 getRecItemR = do
